0573e8a3de805663af4c751dfdc969f817bd6449
chunhan2451 <chun.han.hui@gmail.com>
2025-03-16 17:11:24 +0800

    fix: reduce load of components

--- components/Convert/index.js
+++ components/Convert/index.js
@@ -1,8 +1,4 @@
 // components/Convert/index.js
-// Import all converter components
-import BaseConverter from './BaseConverter.vue';
-import CategoryCard from './CategoryCard.vue';
-
 // Dynamically import category-specific converters to prevent errors
 // when components haven't been created yet
 import { defineAsyncComponent } from 'vue';
@@ -10,47 +6,44 @@ import { defineAsyncComponent } from 'vue';
 // Map of category IDs to their component loaders
 const converterComponents = {
     // Unit-based converters
-    angle: () => import('./Units/AngleConverter.vue').catch(() => BaseConverter),
-    area: () => import('./Units/AreaConverter.vue').catch(() => BaseConverter),
-    color: () => import('./Units/ColorConverter.vue').catch(() => BaseConverter),
-    css: () => import('./Units/CssConverter.vue').catch(() => BaseConverter),
-    data: () => import('./Units/DataConverter.vue').catch(() => BaseConverter),
-    energy: () => import('./Units/EnergyConverter.vue').catch(() => BaseConverter),
-    length: () => import('./Units/LengthConverter.vue').catch(() => BaseConverter),
-    pressure: () => import('./Units/PressureConverter.vue').catch(() => BaseConverter),
-    speed: () => import('./Units/SpeedConverter.vue').catch(() => BaseConverter),
-    temperature: () => import('./Units/TemperatureConverter.vue').catch(() => BaseConverter),
-    time: () => import('./Units/TimeConverter.vue').catch(() => BaseConverter),
-    volume: () => import('./Units/VolumeConverter.vue').catch(() => BaseConverter),
-    weight: () => import('./Units/WeightConverter.vue').catch(() => BaseConverter),
+    angle: () => import('./Units/AngleConverter.vue'),
+    area: () => import('./Units/AreaConverter.vue'),
+    color: () => import('./Units/ColorConverter.vue'),
+    css: () => import('./Units/CssConverter.vue'),
+    data: () => import('./Units/DataConverter.vue'),
+    energy: () => import('./Units/EnergyConverter.vue'),
+    length: () => import('./Units/LengthConverter.vue'),
+    pressure: () => import('./Units/PressureConverter.vue'),
+    speed: () => import('./Units/SpeedConverter.vue'),
+    temperature: () => import('./Units/TemperatureConverter.vue'),
+    time: () => import('./Units/TimeConverter.vue'),
+    volume: () => import('./Units/VolumeConverter.vue'),
+    weight: () => import('./Units/WeightConverter.vue'),
 
     // Text & Data converters
-    'text-data': () => import('./TextDataTools/Text/Base64Converter.vue').catch(() => BaseConverter),
-    base64: () => import('./TextDataTools/Text/Base64Converter.vue').catch(() => BaseConverter),
-    url: () => import('./TextDataTools/Text/UrlEncoder.vue').catch(() => BaseConverter),
-    json: () => import('./TextDataTools/Text/JsonFormatter.vue').catch(() => BaseConverter),
-    csv: () => import('./TextDataTools/Text/CsvConverter.vue').catch(() => BaseConverter),
+    'text-data': () => import('./TextDataTools/Text/Base64Converter.vue'),
+    base64: () => import('./TextDataTools/Text/Base64Converter.vue'),
+    url: () => import('./TextDataTools/Text/UrlEncoder.vue'),
+    json: () => import('./TextDataTools/Text/JsonFormatter.vue'),
+    csv: () => import('./TextDataTools/Text/CsvConverter.vue'),
 
     // Number system converters
-    'number-systems': () => import('./TextDataTools/Number/BinaryConverter.vue').catch(() => BaseConverter),
-    binary: () => import('./TextDataTools/Number/BinaryConverter.vue').catch(() => BaseConverter),
-    roman: () => import('./TextDataTools/Number/RomanConverter.vue').catch(() => BaseConverter),
+    'number-systems': () => import('./TextDataTools/Number/BinaryConverter.vue'),
+    binary: () => import('./TextDataTools/Number/BinaryConverter.vue'),
+    roman: () => import('./TextDataTools/Number/RomanConverter.vue'),
 
     // Time & Date converters
-    'time-date': () => import('./TextDataTools/Time/TimestampConverter.vue').catch(() => BaseConverter),
-    timestamp: () => import('./TextDataTools/Time/TimestampConverter.vue').catch(() => BaseConverter),
-    timezone: () => import('./TextDataTools/Time/TimezoneConverter.vue').catch(() => BaseConverter),
-    duration: () => import('./TextDataTools/Time/DurationConverter.vue').catch(() => BaseConverter),
+    'time-date': () => import('./TextDataTools/Time/TimestampConverter.vue'),
+    timestamp: () => import('./TextDataTools/Time/TimestampConverter.vue'),
+    timezone: () => import('./TextDataTools/Time/TimezoneConverter.vue'),
+    duration: () => import('./TextDataTools/Time/DurationConverter.vue'),
 
     // Text transformation
-    'text-transform': () => import('./TextDataTools/Text/CaseConverter.vue').catch(() => BaseConverter),
-    case: () => import('./TextDataTools/Text/CaseConverter.vue').catch(() => BaseConverter),
-    stats: () => import('./TextDataTools/Text/TextStats.vue').catch(() => BaseConverter),
-    lines: () => import('./TextDataTools/Text/LineTools.vue').catch(() => BaseConverter),
+    'text-transform': () => import('./TextDataTools/Text/CaseConverter.vue'),
+    case: () => import('./TextDataTools/Text/CaseConverter.vue'),
+    stats: () => import('./TextDataTools/Text/TextStats.vue'),
+    lines: () => import('./TextDataTools/Text/LineTools.vue'),
 };
-
-// Export base components
-export { BaseConverter, CategoryCard };
 
 // Helper function to get a converter component by category ID
 export function getConverterByCategory(categoryId) {
